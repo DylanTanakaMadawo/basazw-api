9ec4e92557bcd20aa71f06d8b41e36e2af488f27
DylanTanakaMadawo <dylantanakamadawo@gmail.com>
2023-11-24 01:09:17 +0200

    set header commit

--- index.js
+++ index.js
@@ -16,25 +16,32 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-// dotenv.config();
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", [
+    "https://basazw.onrender.com/",
+    "http://localhost:3000",
+  ]);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(
   cors({
     credentials: true,
     origin: ["https://basazw.onrender.com/", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
-    // optionsSuccessStatus: 200,
+    optionsSuccessStatus: 200,
   })
 );
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
